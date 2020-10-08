18dd03f3b1c2f3d3969f5e377bb99c0d9674cbf5
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2020-10-08 22:14:59 +0700

    them bai tap convertmoney

---
+++ chuyendoitiente/convertMoney.js
@@ -0,0 +1,15 @@
+function myFunction(){
+  let x = document.getElementById("amount").value;
+  alert("so tien la :" +x);
+}
+
+  /*var y=document.getElementById("from").value;
+  var z=document.getElementById("to").value;
+  if( y==1 && z==1){
+     alert("Số tiền không thay đổi");}
+     else {if(y==1){
+        alert("Số tiền là :"+ usd );}
+          else{ alert("Số tiền là :" + vnd)}
+  } 
+   }
+}
